bc06138420526f4e965525d6ec9a34912d0feef8
Tamerlan <tamerlan.abilov95@gmail.com>
2025-09-26 23:54:05 +0400

    retry

--- controllers/auth.go
+++ controllers/auth.go
@@ -960,7 +960,7 @@ func (m *AuthController) ProfileRoutes(g *echo.Group) {
 			return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Failed to save your avatar"})
 		}
 		return c.JSON(http.StatusOK, map[string]string{"message": "Avatar is updated successfully", "upload_url": uploadUrl, "processing_status": user.FullBodyAvatarStatus, "file_name": *req.FileName})
-	}, echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))), UserMiddleware)
+	}, echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))), UserOnlyMiddleware)
 
 	g.POST("/body-characteristic", func(c echo.Context) error {
 		// Get user from context
